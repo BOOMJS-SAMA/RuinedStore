fd49d806d1b9cc04ae6f15682595dcf817fa02f8
YoungAC <1936550353@qq.com>
2021-07-19 15:37:39 +0800

    Add files via upload

---
+++ src/main/java/com/ruined/store/RuinedStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ruined.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuinedStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuinedStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruined/store/RuinedStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruined.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuinedStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
